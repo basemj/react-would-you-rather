191b89d03a641616c693033555bbdcf35e1aff05
basemj <basem.jawahri@gmail.com>
2020-05-09 17:09:25 +0100

    feat: style leaderboard

--- src/components/LeadersList.js
+++ src/components/LeadersList.js
@@ -1,18 +1,41 @@
 import PropTypes from "prop-types";
 import React from 'react';
 import { connect } from 'react-redux';
+import { Card } from "react-bootstrap";
+import styles from "./leadersList.module.css";
 
 const LeadersList = ({orderedUsers}) => {
   return (
-    <ul>
+    <ul className={styles.leadersList}>
       {
         orderedUsers.map((user) => {
           return (
             <li key={user.id}>
-              <img src={`/assets/${user.avatarURL}`} alt="" />
-              {user.name}
-              {user.asked.length}
-              {user.answered.length}
+              <Card>
+                <Card.Header>
+                  <img 
+                    className={styles.avatar}
+                    src={`/assets/${user.avatarURL}`}
+                    alt={`${user.name}'s avatar`}
+                  />
+                  {user.name}
+                </Card.Header>
+                <Card.Body>
+                  <p>
+                    <span>Questions asked: </span>
+                    <b> 
+                      {' '}
+                      {user.asked.length}
+                    </b>
+                  </p>
+                  <p>
+                    <span>Questions answered: </span>
+                    <b> 
+                      {user.answered.length}
+                    </b>
+                  </p>
+                </Card.Body>
+              </Card>
             </li>
           );
         })
